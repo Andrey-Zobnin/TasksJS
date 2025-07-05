f336891b1cdc91a360e981858995061f2cdb5865
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-07-05 07:34:32 +0300

    Уровень 1

--- ЗадачиПервогоБлока/Task3.js
+++
@@ -1,5 +0,0 @@
-// Дана строка. Выведите в консоль последний символ строки.
-
-let string = "string";
-let lastChar = string.length - 1;
-alert(str[str.length - 1]);
--- ЗадачиПервогоБлока/Task6.js
+++
@@ -1,8 +0,0 @@
-// Дано слово. Получите его последнюю букву. Если слово заканчивается на мягкий знак, то получите предпоследнюю букву.?
-let str = "stringь";
-
-if (str[str.length - 1] === "ь") {
-    console.log(str[str.length - 2]);
-} else {
-    console.log(str[str.length - 1]);
-}
